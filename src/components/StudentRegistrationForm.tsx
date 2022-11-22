08f7943713eebca953efe88fe4c50dbab6996eb8
Sergej Grilborzer <sergej@grilborzer.de>
2022-11-22 14:13:35 +0100

    Creat StudentRegistrationForm

---
+++ src/components/StudentRegistrationForm.tsx
@@ -0,0 +1,16 @@
+export default function StudentRegistrationForm() {
+
+    return (
+        <section>
+            {/* Formular Tag: */}
+            <form>
+                <label>
+                    Name:
+                    <input
+
+                    />
+                </label>
+            </form>
+        </section>
+    )
+}
